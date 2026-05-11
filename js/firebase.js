6a8521ce41dcfa7fc41c4164a55101e6b117e408
DanilsonJsD <d97.dias@gmail.com>
2026-05-11 15:42:45 -0300

    teste de botão voltar

---
+++ js/firebase.js
@@ -0,0 +1,40 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// CONFIG FIREBASE
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCpDANdwBksAfDAgvhff_9zDEOUdvA4oTg",
+
+  authDomain: "dav-lais.firebaseapp.com",
+
+  projectId: "dav-lais",
+
+  storageBucket: "dav-lais.firebasestorage.app",
+
+  messagingSenderId: "669039734911",
+
+  appId: "1:669039734911:web:6e7245343d37e3b80f4f4a"
+};
+
+// INICIA FIREBASE
+const app = initializeApp(firebaseConfig);
+
+// INICIA FIRESTORE
+const db = getFirestore(app);
+
+// EXPORTA
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs
+};
