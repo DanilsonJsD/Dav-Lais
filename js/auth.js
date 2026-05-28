a18bd02c78f5fee7b7db6d5ca210f4e71e75bed7
DanilsonJsD <d97.dias@gmail.com>
2026-05-28 19:47:01 -0300

    criação da regra de login com o usuário

--- js/auth.js
+++ js/auth.js
@@ -1,33 +1,122 @@
+import {
+
+    db,
+
+    collection,
+
+    getDocs
+
+} from "./firebase.js";
+
+
+// =========================
+// ELEMENTOS
+// =========================
+
+const email =
+document.getElementById("email");
+
+const senha =
+document.getElementById("senha");
+
 const btnLogin =
 document.getElementById("btnLogin");
 
 
+// =========================
+// LOGIN
+// =========================
+
 btnLogin.addEventListener(
+
     "click",
-    fazerLogin
+
+    login
 );
 
 
-function fazerLogin(){
-
-    const email =
-    document.getElementById("email").value;
-
-    const senha =
-    document.getElementById("senha").value;
-
+async function login(){
 
     if(
-        email === "d97.dias@gmail.com"
-        &&
-        senha === "123"
+
+        email.value === "" ||
+
+        senha.value === ""
+
     ){
 
-        window.location.href =
-        "pages/dashboard.html";
+        alert(
+            "Preencha os campos"
+        );
 
-    }else{
+        return;
+    }
 
-        alert("E-mail ou senha inválidos");
+
+    try{
+
+        const querySnapshot =
+        await getDocs(
+            collection(db, "usuarios")
+        );
+
+
+        let usuarioEncontrado =
+        null;
+
+
+        querySnapshot.forEach((docItem) => {
+
+            const usuario =
+            docItem.data();
+
+
+            if(
+
+                usuario.email === email.value &&
+
+                usuario.senha === senha.value
+
+            ){
+
+                usuarioEncontrado = {
+
+                    id: docItem.id,
+
+                    ...usuario
+                };
+            }
+        });
+
+
+        if(usuarioEncontrado){
+
+            localStorage.setItem(
+
+                "usuarioLogado",
+
+                JSON.stringify(
+                    usuarioEncontrado
+                )
+            );
+
+
+            window.location.href =
+            "dashboard.html";
+
+        }else{
+
+            alert(
+                "Email ou senha inválidos"
+            );
+        }
+
+    }catch(error){
+
+        console.log(error);
+
+        alert(
+            "Erro login"
+        );
     }
 }
